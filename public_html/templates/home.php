c12c1687e3b7c0cfa66e7efe827bb11b355c2967
Jordan Vinson <jvinson3@cnm.edu>
2016-11-17 16:23:25 -0700

    Changed text

--- public_html/templates/home.php
+++ public_html/templates/home.php
@@ -1,8 +1,8 @@
 <section class="welcome">
 	<div class="container">
 		<div class="jumbotron text-center">
-			<h1>Welcome to the Angular 2 Demo!</h1>
-			<p class="lead">This is a bare-bones Angular 2 Front End :D</p>
+			<h1>Someone get me an easy button</h1>
+			<p class="lead">Cuz this shit is easy (Not really this took 45 minutes for just this)</p>
 		</div>
 	</div>
 </section>
